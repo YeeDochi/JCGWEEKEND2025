4996258d5c522378665fc320a365ef83c31639e3
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-16 14:17:33 +0900

    03.16

--- Jungol/src/Q511/Main.java
+++ Jungol/src/Q511/Main.java
@@ -1,17 +1,17 @@
 package Q511;
 
 
-import java.util.Scanner;
-
-public class Main {
-	public static void main(String[] args) {
-       int a,b;
-       a=55;
-       b=10;
-       System.out.printf("%d - %d = %d\n",a,b,a-b);
-        a=2008;
-       b=1999;
-       System.out.printf("%d - %d = %d\n",a,b,a-b);
-     
+	import java.util.Scanner;
+	
+	public class Main {
+		public static void main(String[] args) {
+	       int a,b;
+	       a=55;
+	       b=10;
+	       System.out.printf("%d - %d = %d\n",a,b,a-b);
+	       a=2008;
+	       b=1999;
+	       System.out.printf("%d - %d = %d\n",a,b,a-b);
+	     
+		}
 	}
-}
